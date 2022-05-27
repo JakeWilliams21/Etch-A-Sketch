9df5245da076af68109bf49132717349b6b0033d
Jake Williams <jakewilliams530@gmail.com>
2022-05-26 19:28:03 -0600

    Re-worked Javascript, Added more structure to index.html, added reset functionality,

--- app.js
+++ app.js
@@ -1,17 +1,62 @@
-const container = document.querySelector('.container');
+const gridLabel = document.querySelector('h3')
+const board = document.querySelector('.board');
+const apply = document.querySelector('.apply')
+const resetButton = document.querySelector('.reset')
+
+resetButton.addEventListener('click', () => {
+    wipe();
+})
+
+apply.addEventListener('click', size = () => {
+    const dimension = document.querySelector('input')
+    console.log(dimension.value);
+    reset();
+    createBoard(dimension.value);
+    
+})
+
+const createBoard = (gridSize) => {
+
+const tileAmount = gridSize * gridSize
+    gridLabel.textContent = `${gridSize} x ${gridSize}`
+    board.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
+    board.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`
+    board.style.border = 'solid'
 
 
-
-for (i = 0; i <  256; i++) {
-    const box = document.createElement('div');
-    box.classList.add('box');
-    box.id = i
-    container.appendChild(box)
+for (i = 0; i < tileAmount; i++) {
+    let tile = document.createElement('div')
+        tile.style.border = 'solid'
+        tile.id = i
+        tile.classList.add('tile')
+        board.appendChild(tile);
 }
-const boxes = document.querySelectorAll('.box')
+const tiles = document.querySelectorAll('.tile')
 
-for (i = 0; i < boxes.length; i++) {
-    boxes[i].addEventListener('mouseenter', e => {
-        boxes[e.target.id].style.backgroundColor = 'blue'
+
+for (i = 0; i < tiles.length; i++) {
+    tiles[i].addEventListener('mouseenter', e => {
+        tiles[e.target.id].style.backgroundColor = 'blue'
     })
 }
+
+}
+
+const wipe = () => {
+    const tiles = document.querySelectorAll('.tile')
+
+    for(i = 0; i < tiles.length; i++) {
+        tiles[i].style.backgroundColor = 'white'
+    }
+}
+
+const reset = () => {
+    const tiles = document.querySelectorAll('.tile')
+    
+for (i = 0; i < tiles.length; i++) {
+    board.removeChild(tiles[i])
+}
+    
+}
+
+createBoard(16);
