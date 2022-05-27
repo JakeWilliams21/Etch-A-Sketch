58158117bdd369ee318c5dda49e5f3ef5b2a5d5c
Jake Williams <jakewilliams530@gmail.com>
2022-05-26 19:36:14 -0600

    Added randomColor Functionality

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const gridLabel = document.querySelector('h3')
 const board = document.querySelector('.board');
 const apply = document.querySelector('.apply')
 const resetButton = document.querySelector('.reset')
+const randomButton = document.querySelector('.random')
+
+randomButton.addEventListener('click', () => {
+    randomColor();
+})
 
 resetButton.addEventListener('click', () => {
     wipe();
@@ -26,7 +31,6 @@ const tileAmount = gridSize * gridSize
 
 for (i = 0; i < tileAmount; i++) {
     let tile = document.createElement('div')
-        tile.style.border = 'solid'
         tile.id = i
         tile.classList.add('tile')
         board.appendChild(tile);
@@ -36,10 +40,25 @@ const tiles = document.querySelectorAll('.tile')
 
 for (i = 0; i < tiles.length; i++) {
     tiles[i].addEventListener('mouseenter', e => {
-        tiles[e.target.id].style.backgroundColor = 'blue'
+        tiles[e.target.id].style.backgroundColor = 'black'
     })
 }
 
+}
+
+const randomColor = () => {
+    const tiles = document.querySelectorAll('.tile')
+
+    for (i = 0; i < tiles.length; i++) {
+        const x = Math.floor(Math.random() * 256)
+        const y = Math.floor(Math.random() * 256)
+        const z = Math.floor(Math.random() * 256)
+        const bgColor = `rgb(${x},${y},${z})`
+        console.log(bgColor);
+        tiles[i].addEventListener('mouseenter', e => {
+            tiles[e.target.id].style.backgroundColor = bgColor
+        })
+    }
 }
 
 const wipe = () => {
